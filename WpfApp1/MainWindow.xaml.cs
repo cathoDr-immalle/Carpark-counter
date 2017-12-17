9be47a70b3a5a1f1887fae76931b926dbc795437
cathoDr-immalle <catho.driesen@immalle.be>
2017-12-17 12:07:52 +0100

    code for counting cars in a carpark

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
@@ -20,9 +21,44 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int carCounter = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+           
         }
-    }
+
+        private void ButtonEntre_Click(object sender, RoutedEventArgs e)
+        {
+            int vol = 20;
+            carCounter = carCounter + 1;
+            
+
+            if (carCounter > vol)
+            {
+                MessageBox.Show("The car park is full", "No space", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                LabelCounter.Content = Convert.ToString(carCounter);
+            }
+        }
+
+        private void ButtonLeave_Click(object sender, RoutedEventArgs e)
+        {
+            int leeg = 0;
+            carCounter = carCounter - 1;
+
+
+            if (carCounter < leeg)
+            {
+                MessageBox.Show("The car park is empty, there are no cars who can leave annymore!", "No cars", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                LabelCounter.Content = Convert.ToString(carCounter);
+            }
+        }
+    }   
 }
